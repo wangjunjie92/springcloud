367e6ab6c7acde17d7664724bb1dddb1319f228b
wangjunjie <122077774@qq.com>
2019-04-04 15:04:05 +0800

    修改README.md2

---
+++ bfxy-order/order-server/src/main/java/com/wjj/cloud/order/server/common/IdUtil.java
@@ -0,0 +1,10 @@
+package com.wjj.cloud.order.server.common;
+
+/**
+ * @Author: wangjunjie 2019/4/4 15:01
+ * @Description: 订单id生成工具类
+ * @Version: 1.0.0
+ * @Modified By: xxx 2019/4/4 15:01
+ */
+public class IdUtil {
+}
---
+++ bfxy-order/order-server/src/main/java/com/wjj/cloud/order/server/config/RedisConfig.java
@@ -0,0 +1,49 @@
+package com.wjj.cloud.order.server.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @Author: wangjunjie 2018/8/17 10:19
+ * @Description:
+ * @Version: 1.0.0
+ * @Modified By: xxx 2018/8/17 10:19
+ */
+
+//@Configuration
+//@EnableCaching
+public class RedisConfig {
+
+
+
+    @Bean
+    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>
+                (Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL,JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+
+        return redisTemplate;
+
+    }
+}
